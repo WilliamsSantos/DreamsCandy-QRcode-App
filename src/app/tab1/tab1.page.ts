534a1198478986468a5e1d809efaee8cf98e6fbb
Williams Santos <Williamssantos1996@outlook.com>
2019-08-15 16:03:50 -0300

    Desenvolvimento de telas inicial: concluida

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,18 +14,20 @@ export class Tab1Page {
   constructor() {}
 
   busca(){
+
     const searchbar = document.querySelector('ion-searchbar');
-    const items = Array.from(document.querySelector('ion-list').children);
+    const items = Array.from( document.querySelector('ion-list').children );
     searchbar.addEventListener('ionInput', handleInput);
-    function handleInput(event) {
+
+    function handleInput( event ) {
       const query = event.target.value.toLowerCase();
-      requestAnimationFrame(() => {
-        items.forEach(item => {
-          const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
-          item.style.display = shouldShow ? 'block' : 'none';
+
+      requestAnimationFrame( () => {
+        items.forEach( item => {
+          const shouldShow    = item.textContent.toLowerCase().indexOf( query ) > -1;
+          item.style.display  = shouldShow ? 'block' : 'none';
         });
       });
     }
   }
-  
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +9,46 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  loading:any;
 
+  constructor(
+    public loadingController: LoadingController,
+    public alertController: AlertController
+  ) {}
+
+  async sincronizarDados(){
+    this.loading = await this.loadingController.create({
+      spinner:'dots',
+      message: 'Sincronizando',
+      translucent: true,
+      cssClass: 'b-color'
+    }); await this.loading.present();
+
+    if ( this.submeterDados('Guardando') ){
+      this.loading.style.display = 'none';
+    }
+  }
+
+  //Função desenvolvida apenas para fins de teste
+  goHome(){
+    window.location.href='/login'
+  }
+
+
+  async submeterDados( mensagem ){
+    this.alerta()
+  }
+
+  async alerta(){
+    const alert = await this.alertController.create({
+      header: 'Tudo Sincronizado!',
+      message: 'Seus Dados Já Estão Atualizados.',
+      buttons: [
+        {
+          text: 'Entendi',
+        }
+      ],
+    });
+  await alert.present();
+  }
 }
