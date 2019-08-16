b58d28f3db2577f2ae07725b17e3998abedda94d
Williams Santos <Williamssantos1996@outlook.com>
2019-08-16 10:52:22 -0300

    Criação modal informações inscritos = Concluido

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,7 +12,7 @@ export class Tab1Page {
   searchbar:any;
   items:any;
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
   busca(){
     const searchbar = document.querySelector('ion-searchbar');
@@ -28,6 +29,70 @@ export class Tab1Page {
         });
       });
     }
+  }
+
+  async checkInscrito(){
+      const alert = await this.alertController.create({
+        header: 'Pedro',
+        inputs: [
+          {
+            label: 'CPF:',
+            type: 'text',
+            value: 'CPF : 15225488744',
+            disabled: true
+          },
+          // input date with min & max
+          {
+            name: 'lote',
+            value: 'LOTE : Aqui vai o tipo do lote',
+            disabled: true
+          },
+          {
+            name: 'data-lote-inscricao',
+            type: 'text',
+            value: 'INSCRIÇÃO : 16/08/2019',
+            disabled: true
+          },
+          // input date without min nor max
+          {
+            name: 'periodo',
+            type: 'text',
+            value: 'PERIODO LOTE:',
+            disabled: true
+          },
+          // input date without min nor max
+          {
+            name: 'data-lote-inicio',
+            type: 'text',
+            value: 'INICIO : 16/08/2019',
+            disabled: true
+          },
+          {
+            name: 'data-lote-vencimento',
+            type: 'text',
+            value: 'VENCIMENTO : 16/08/2019',
+            disabled: true
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+              return false
+            }
+          }, {
+            text: 'Confirmar',
+            handler: () => {
+              console.log('Confirm Ok');
+              return true
+            }
+          }
+        ]
+      });
+    await alert.present();
   }
 
   salvarConfirmado( c_input:boolean ){
